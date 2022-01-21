93eccb94222558bb5da52470554a826eebe4f4ad
mealybug-gh <84744150+mealybug-gh@users.noreply.github.com>
2022-01-21 22:26:13 +0100

    Update message-limit.js

--- rooms/page/js/app/plugin/message-limit.js
+++ rooms/page/js/app/plugin/message-limit.js
@@ -1,11 +1,11 @@
 var CandyMessageLimit = (function(self, Candy, $) {
-	self.events.candy.onMessageFormInputAdded = function(e, args) {
+	self.onMessageFormInputAdded = function(e, args) {
 		window.console.log('setting max length for message form input');
 		$('input[name="message"]').attr('maxlength', '250');
 	};
 	self.init = function() {
-    	$(Candy).bind('candy:view.room.after-add', self.events.candy.onMessageFormInputAdded);
-		$(Candy).bind('candy:view.room.after-show', self.events.candy.onMessageFormInputAdded);
+    	$(Candy).bind('candy:view.room.after-add', self.onMessageFormInputAdded);
+		$(Candy).bind('candy:view.room.after-show', self.onMessageFormInputAdded);
     };
 	return self;
 }(CandyMessageLimit || {}, Candy, jQuery));
